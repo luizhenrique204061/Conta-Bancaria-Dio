63fbb53ae6a817005ff214eda10abe79591363e7
Luiz Henrique <luizhenrique204061@gmail.com>
2024-08-26 01:28:14 -0300

    Segundo Commit

---
+++ src/ContaBanco.java
@@ -0,0 +1,48 @@
+import java.util.*;;
+
+public class ContaBanco {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = 0;
+        String agencia = "";
+        String nomeCliente = "";
+        double saldo = 1000.0;
+
+        //Validação da Agência
+        while (true) {
+            System.out.println("Por favor digite o número da agência");
+            if (scanner.hasNext()) {
+                agencia = scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Erro, digite o número da sua agência. Exemplo: 014-7");
+            }
+        } 
+        //Validação da conta bancária
+        while (true) {
+            System.out.println("Digite o número da sua conta bancária.");
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                scanner.nextLine();
+                break;
+            } else {
+                System.out.println("Erro, digite o número da sua conta bancária. Exemplo: 1004");
+            }
+        }
+
+        //Validação do nome cliente
+        while (true) {
+            System.out.println("Digite o seu nome.");
+            nomeCliente = scanner.nextLine();
+            if (nomeCliente.matches("[A-Za-z ]+")) {
+            
+                break;
+            } else {
+                System.out.println("Erro digite um nome válido. Exemplo: Carla, Rodrigo, Luiz");
+            }
+        }
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
